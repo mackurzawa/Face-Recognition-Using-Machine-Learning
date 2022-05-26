eebb496cd48233e1929f84d47e87ba053f19fbd6
mackurzawa <mac.kurzawa@gmail.com>
2022-05-26 23:02:52 +0200

    Emojis added

--- Real time emotions/main.py
+++ Real time emotions/main.py
@@ -1,31 +1,46 @@
-from cv2 import VideoCapture, resize, COLOR_BGR2GRAY, cvtColor, CascadeClassifier, rectangle
+from cv2 import VideoCapture, resize, COLOR_BGR2GRAY, cvtColor, CascadeClassifier, rectangle, imshow, waitKey, destroyAllWindows, imread
 import numpy as np
 from matplotlib import pyplot as plt
 from keras.models import load_model
 import tensorflow as tf
 # from skimage import transform
 
+new_class_labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+
+
+emojis = [resize(imread(f'Emojis/{x}.png'), (500, 500)) for x in new_class_labels]
+last_predictions = [4 for _ in range(20)]
+
 
 vid = VideoCapture(0)
-
+face_cascade = CascadeClassifier('../haarcascade_frontalface_default.xml')
+my_model = load_model('../my_model_face_recognition_100_epochs.h5')
 while vid.isOpened():
     ret, frame = vid.read()
-    frame = cvtColor(frame, COLOR_BGR2GRAY)
-    face_cascade = CascadeClassifier('../haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+    frame_bw = cvtColor(frame, COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(frame_bw, 1.1, 4)
     for (x, y, w, h) in faces:
-        rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        face = frame[y:y + h, x:x + w]
+        rectangle(frame_bw, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face = frame_bw[y:y + h, x:x + w]
     face = resize(face, (48, 48))
-    my_model = load_model('../my_model_face_recognition_100_epochs.h5')
-    # face_r = resize(face, (48, 48))
     face_r = np.expand_dims(face, axis=0)
     face_r = face_r / 255.0
     prediction = my_model.predict(face_r)
     predictions_temp = np.argmax(prediction, axis=1)
-    new_class_labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
     prediction = np.argmax(prediction)
-    prediction = new_class_labels[prediction]
-    plt.imshow(face, cmap='gray')
-    plt.title(prediction)
-    plt.show()
+    last_predictions.append(prediction)
+    del last_predictions[0]
+    mean_prediction = max(set(last_predictions), key=last_predictions.count)
+
+    imshow('VIDEOCAM', frame)
+    imshow('EMOJI', emojis[mean_prediction])
+    waitKey(1)
+    # if waitKey(1) == 27:
+    #     destroyAllWindows()
+
+
+    # destroyAllWindows()
+
+    # plt.imshow(frame, cmap='gray')
+    # plt.title(prediction)
+    # plt.show()
